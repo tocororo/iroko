112e9c52d8a60a5e49c5b99fee6896dc324d785b
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2019-12-02 08:20:00 -0500

    terms api

--- iroko/taxonomy/api.py
+++ iroko/taxonomy/api.py
@@ -20,32 +20,30 @@ class Vocabularies:
 
     @classmethod
     def edit_vocabulary(cls, id ,data) -> Dict[str, Vocabulary]:
-        
+
         msg, vocab = cls.get_vocabulary(id)
         if vocab:
-            # data = vocabulary_schema.loads(input_data)
             vocab.name = data['name']
             vocab.description = data['description']
-            if 'data' in data: 
+            if 'data' in data:
                 vocab.data = data['data']
             db.session.commit()
-            msg = 'New Vocabulary UPDATED id={0}'.format(vocab.id)
+            msg = 'New Vocabulary UPDATED name={0}'.format(vocab.name)
         return msg, vocab
 
     @classmethod
     def new_vocabulary(cls, data) -> Dict[str, Vocabulary]:
 
-        # data = vocabulary_schema.loads(input_data)
         vocab = Vocabulary.query.filter_by(name=data['name']).first()
         if not vocab:
             vocab = Vocabulary()
             vocab.name = data['name']
             vocab.description = data['description']
-            if 'data' in data: 
+            if 'data' in data:
                 vocab.data = data['data']
             db.session.add(vocab)
             db.session.commit()
-            msg = 'New Vocabulary CREATED id={0}'.format(vocab.id)
+            msg = 'New Vocabulary CREATED name={0}'.format(vocab.name)
             return msg, vocab
         else:
             msg = 'Vocabulary already exist name={0}'.format(data['name'])
@@ -56,10 +54,43 @@ class Vocabularies:
 class Terms:
     """Manage Terms"""
 
-    @classmethod
-    def get_term(id):
-        print('not implemented yet')
 
     @classmethod
-    def new_term(cls, input_data):
-        print('not implemented yet')
+    def get_term(cls, id) -> Dict[str, Term]:
+        term = Term.query.filter_by(uuid=id).first()
+        if term:
+            return 'ok', term
+        else:
+            msg = 'Term not exist id={0}'.format(id)
+            return msg, None
+
+    @classmethod
+    def edit_term(cls, id ,data) -> Dict[str, Term]:
+
+        msg, term = cls.get_term(id)
+        if term:
+            term.name = data['name']
+            term.description = data['description']
+            if 'data' in data:
+                term.data = data['data']
+            db.session.commit()
+            msg = 'New Term UPDATED name={0}'.format(term.name)
+        return msg, term
+
+    @classmethod
+    def new_term(cls, data) -> Dict[str, Term]:
+
+        term = Term.query.filter_by(name=data['name']).first()
+        if not term:
+            term = Term()
+            term.name = data['name']
+            term.description = data['description']
+            if 'data' in data:
+                term.data = data['data']
+            db.session.add(term)
+            db.session.commit()
+            msg = 'New Term CREATED id={0}'.format(term.id)
+            return msg, term
+        else:
+            msg = 'Term already exist name={0}'.format(data['name'])
+            return msg, None
