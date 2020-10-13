f458d5137340463565867a47ab55c4c29afbe6a7
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2020-10-13 15:00:38 -0400

    no task

--- iroko/harvester/tasks.py
+++ iroko/harvester/tasks.py
@@ -3,31 +3,26 @@
 
 from __future__ import absolute_import, print_function
 
-from celery import shared_task
-
-from iroko.harvester.oai.harvester import OaiHarvester
-from iroko.sources.models import Source
-
-
 # from iroko.harvester.signals import harvest_finished
-
-@shared_task
-def test_task(source):
-
-    harvester = OaiHarvester(source)
-    # iterator.get_identifiers()
-    # iterator.get_all_metadata()
-
-@shared_task
-def harvest_source_task(source_id, work_remote=True, request_wait_time=3):
-    source = Source.query.filter_by(id=source_id).first()
-    for i in [0,1000000]:
-        pass
-        # print(str(i))
-    # harvest_finished.send(source)
-    # source = Source.query.filter_by(id=source_id).first()
-    #     if source is not None:
-    #         harvester = OaiHarvester(source, work_remote=work_remote, request_wait_time=4)
-    #         harvester.identity_source()
-    #         harvester.discover_items()
-    #         harvester.process_items()
+#
+# @shared_task
+# def test_task(source):
+#     pass
+#     # harvester = OaiHarvester(source)
+#     # iterator.get_identifiers()
+#     # iterator.get_all_metadata()
+#
+# @shared_task
+# def harvest_source_task(source_id, work_remote=True, request_wait_time=3):
+#     pass
+#     # source = Source.query.filter_by(id=source_id).first()
+#     # for i in [0,1000000]:
+#     #     pass
+#         # print(str(i))
+#     # harvest_finished.send(source)
+#     # source = Source.query.filter_by(id=source_id).first()
+#     #     if source is not None:
+#     #         harvester = OaiHarvester(source, work_remote=work_remote, request_wait_time=4)
+#     #         harvester.identity_source()
+#     #         harvester.discover_items()
+#     #         harvester.process_items()
