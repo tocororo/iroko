50016102b0132388dbfe768a95a1f4e9c4025475
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-03-17 02:01:56 -0400

    dep

--- iroko/config.py
+++ iroko/config.py
@@ -5,6 +5,7 @@
 #  SCEIBA is free software; you can redistribute it and/or modify it
 #  under the terms of the MIT License; see LICENSE file for more details.
 #
+#
 
 
 """Default configuration for iroko.
