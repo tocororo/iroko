bb6ffc5786dc7b774a9fee0d9c20bcfb26326709
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-10-14 23:56:45 -0400

    theme problemrm

--- iroko/theme/webpack.py
+++ iroko/theme/webpack.py
@@ -14,23 +14,15 @@ theme = WebpackThemeBundle(
     'assets',
     default='semantic-ui',
     themes={
-        'bootstrap3': dict(
-            entry={
-                'testinv-theme': './scss/testinv/theme.scss',
-                'testinv-preview': './js/testinv/previewer.js',
-            },
-            dependencies={},
-            aliases={},
-        ),
         'semantic-ui': dict(
             entry={
-                'testinv-preview': './js/testinv/previewer.js',
+                'testinv-preview': './js/iroko/previewer.js',
             },
             dependencies={
                 # add any additional npm dependencies here...
             },
             aliases={
-                '../../theme.config$': 'less/testinv/theme.config',
+                '../../theme.config$': 'less/iroko/theme.config',
             },
         ),
     }
