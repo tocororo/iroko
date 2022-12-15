913214a5298412f8fecd2d82ab71d345adf3bd90
Rafael Martinez Estevez <malayibiri@gmail.com>
2022-12-15 10:10:08 -0500

    add base

---
+++ iroko/persons/marshmallow/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2022. Universidad de Pinar del Rio
+#  This file is part of SCEIBA (sceiba.cu).
+#  SCEIBA is free software; you can redistribute it and/or modify it
+#  under the terms of the MIT License; see LICENSE file for more details.
+
+#
+#
+# Iroko is free software; you can redistribute it and/or modify it under the
+# terms of the MIT License; see LICENSE file for more details.
+
+"""Schemas for marshmallow."""
+
+from __future__ import absolute_import, print_function
+
+from .json import OrgMetadataSchemaV1, OrgMetadataSchemaRelIDsV1, RecordSchemaV1, OrgRecordSearchSchemaV1, OrgMetadataSchemaBaseV1
+
+__all__ = (
+'OrgMetadataSchemaV1', 'OrgMetadataSchemaRelIDsV1', 'RecordSchemaV1', 'OrgRecordSearchSchemaV1',
+'OrgMetadataSchemaBaseV1')
