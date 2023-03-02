a9c2bf02d967336aa74407dcf3c8fccadcc2fda6
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-03-02 15:52:55 -0500

    importfile persons

--- iroko/persons/rest.py
+++ iroko/persons/rest.py
@@ -7,9 +7,12 @@ import datetime
 from iroko.persons.fixtures import allowed_file, csv_to_json, get_ext
 from flask import Blueprint, request, jsonify, abort, current_app, Flask, flash, redirect, url_for, make_response
 from werkzeug.utils import secure_filename
-from iroko.persons.api import PersonRecord 
+from iroko.persons.api import PersonRecord
 from iroko.persons.serializers import json_v1_response
 from iroko.pidstore import pids
+from invenio_oauth2server import require_api_auth
+
+from iroko.utils import IrokoResponseStatus, iroko_json_response
 
 api_blueprint = Blueprint(
     'iroko_api_persons',
@@ -43,34 +46,34 @@ def get_person_by_pid_canonical():
 
 
 
-@api_blueprint.route('/import', methods=['POST'])
-def upload_file():
+@api_blueprint.route('/import/<org_uuid>', methods=['POST'])
+@require_api_auth()
+def upload_file(org_uuid):
     # /tmp/iroko/person/<datetime>.[csv|json]
+    try:
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('No file part')
+                raise Exception("Not file in request")
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                raise Exception("Not file in request")
+            if file and allowed_file(file.filename):
+                if 'csv'==get_ext(file.filename):
+                 json_path=csv_to_json(file)
+                 PersonRecord.load_from_json_file(json_path, org_uuid)
+                 response = make_response(jsonify({'msg': 'success'}))
+                 return response, 201
+                else:
+                    filename=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+'.'+'json'
 
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return 'Not file in request'
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return 'Not file in request'
-        if file and allowed_file(file.filename):
-            if 'csv'==get_ext(file.filename):
-             json_path=csv_to_json(file)
-             PersonRecord.load_from_json_file(json_path,'cd34b342-f608-4b48-b433-e3020c6140c7')
-             response = make_response(jsonify({'msg': 'success'}))
-             return response, 201
-            else:
-                filename=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+'.'+'json'
-                
-                file.save(os.path.join('./data', filename ))
-                PersonRecord.load_from_json_file(os.path.join('./data',filename),'cd34b342-f608-4b48-b433-e3020c6140c7' )
-                response = make_response(jsonify({'msg': 'success'}))
-                return response ,201
-                
+                    file.save(os.path.join('./data', filename ))
+                    PersonRecord.load_from_json_file(os.path.join('./data',filename),org_uuid )
+                    response = make_response(jsonify({'msg': 'success'}))
+                    return response ,201
 
-                
-    return "Not suppoted file"
+    except Exception as e:
+        return iroko_json_response(IrokoResponseStatus.ERROR, str(e), None, None)
